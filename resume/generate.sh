01493b9a28257000a3b41617c827dc799fe8fc4d
luchiniatwork <tiago@luchini.nyc>
2022-07-11 14:56:06 -0400

    Some of the latest changes

--- resume/generate.sh
+++ resume/generate.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-rm *.pdf
+rm -f *.pdf
 docker run --rm -v $(pwd):/src \
        luchiniatwork/beautiful-md \
        sh -c \
        "for f in *.tex; do latex -output-format=pdf \"\$f\"; done"
-rm *.aux
-rm *.out
-rm *.log
-#rm *.log
+rm -f *.aux
+rm -f *.out
+rm -f *.log
